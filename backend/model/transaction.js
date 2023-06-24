6c3bd1766c5640375c06aba3268fd4c9335f1e03
Suphal Athlur <73172088+SuphalAthlur@users.noreply.github.com>
2023-06-24 11:43:26 +0530

    Modified Transaction Schema

--- backend/model/transaction.js
+++ backend/model/transaction.js
@@ -2,17 +2,20 @@ const { Schema } = require("mongoose");
 
 // Transaction schema for MongoDB
 const transactionSchema = new Schema({
-    transaction_description: { type: String, default: null },
     transaction_datetime: { type: String, default: null },
-    transaction_amount: { type: String, default: null },
+    transaction_description: { type: String, default: null },
     transaction_type: { type: String, default: null },
     transaction_category: { type: String, default: null },
-    transaction_status: { type: String, default: null },
+
+    transaction_amount: { type: Number, default: null },
     transaction_currency: { type: String, default: null },
+    transaction_status: { type: String, default: null },
+
     transaction_method: { type: String, default: null },
     transaction_card: { type: String, default: null },
     transaction_bank: { type: String, default: null },
     transaction_merchant: { type: String, default: null },
+    
     transaction_comments: { type: String, default: null },
 });
 
